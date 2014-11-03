4badf0a313a11ceff48e74ae8ffe3036d25af284
Teri Chu <terichu@Teris-MacBook-Pro.local>
2014-11-02 23:06:42 -0500

    added production/database.php

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'foobook',
+            'username'  => 'root',
+            'password'  => 'I9rJdwFwKx,
+        ),
+
+    ),
+
+);
